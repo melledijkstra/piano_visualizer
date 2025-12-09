9b578b18e902db3da6c302dbfe14112eb20c9e9d
Melle Dijkstra <melle.dijkstra@adidas.com>
2025-12-09 23:00:45 +0100

    Refactor HUD to track notes hit and update song info + adjusted background setup

--- hud.gd
+++ hud.gd
@@ -8,18 +8,11 @@ extends CanvasLayer
 # -- State --
 var song: Song = null
 var current_time: float
+var notes_hit: int = 0
 
 func _ready() -> void:
   if song_info.is_node_ready() and song:
     update_song_info()
-
-func _on_song_loaded(song_data: Song) -> void:
-  self.song = song_data
-  update_song_info()
-
-func _on_time_updated(_current_time: float) -> void:
-  current_time = _current_time
-  update_simulation_info()
 
 func update_simulation_info():
   simulation_info.text = "Speed: {speed}\nTime: {time}".format({
@@ -30,6 +23,18 @@ func update_simulation_info():
 func update_song_info():
   if not song_info: return
   song_info.text = "Notes: {notes}\nTotal song time: {song_duration}".format({
-    "notes": str(song.notes.size()),
+    "notes": "%d/%d" % [notes_hit, song.notes.size()],
     "song_duration": song.duration_formatted()
   })
+
+func _on_song_loaded(song_data: Song) -> void:
+  self.song = song_data
+  update_song_info()
+
+func _on_time_updated(_current_time: float) -> void:
+  current_time = _current_time
+  update_simulation_info()
+
+func _on_note_hit_target(_note_data: NoteData) -> void:
+  notes_hit += 1
+  update_song_info()
