c0aed14cc7eb530ec582d05077f06db2d657bff0
Melle Dijkstra <melle.dykstra@gmail.com>
2026-05-11 16:43:10 +0200

    changes to shaders

--- components/note.gd
+++ components/note.gd
@@ -19,10 +19,20 @@ func setup(note_data: NoteData, lane_width: float, fall_speed: float):
 	h = calculate_height()
 	
 	self.size = Vector2(w, h)
+	
+	# Duplicate material to allow unique shader parameters per note
+	if material:
+		self.material = material.duplicate()
+		_update_shader_size()
 
 func calculate_height() -> float:
 	self.size.y = max(data.duration * speed, 2.0)
+	_update_shader_size()
 	return self.size.y
+
+func _update_shader_size() -> void:
+	if material is ShaderMaterial:
+		(material as ShaderMaterial).set_shader_parameter("node_size", self.size)
 
 func bottom_y() -> float:
 	# anchor is at the top left of the note rectangle (position.y)
